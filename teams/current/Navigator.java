a73bc9f92a6101fac6254ddd360f25e27b7d7823
paulmusgrave <musgrave.paul1@gmail.com>
2013-01-11 23:05:13 -0500

    Minimal functional Navigator; untested

--- teams/current/Navigator.java
+++ teams/current/Navigator.java
@@ -6,17 +6,26 @@ Encapsulates all navigation-related functions.
 DOES NOT INCLUDE INTELLIGENT GOAL SETTING.
 
 In our internal representation of the map/costs:
-[object id (if relevant)]  [cost]
+[object id (if relevant)]  [square attributes]
        16 bits             16 bits
-e.g. 0x0002000A = object id 2 with cost 10
+e.g. 0x0002000A = object id 2 with content of type 10
+Current square attributes:
+0x00000001: hostile mine
 *************************************/
 
 //Notes:
-//Stop when at search depth (i.e. edge of localMap)
 //Multiply heuristic cost by 1.01 to increase greediness? Won't break anything
 import battlecode.common.*;
 
-//TODO: Pass alloted bytecodes?
+
+//TODO:@1 other robots
+//TODO: map edges
+
+//TODO: error handling
+//TODO: bytecodes
+//TODO: getter/setter for searchDepth, searchFreq
+//TODO:@5 danger and impatience state variables; will need to replan path when they change
+//TODO: stagger map update and search (update centered on next square)?
 
 public class Navigator {
 	
@@ -27,6 +36,9 @@ public class Navigator {
 	private int mapDiagRSquared = 2*searchDepth*searchDepth;
 	private int mapSize = 4*searchDepth+1; // Needs to be >= 2*floor(sqrt(2)*searchDepth)+1
 	private int offset = (mapSize-1)/2;
+	private int maxPathLength = mapSize; //?
+	
+	private int searchFreq = 3; // Needs to be <= searchDepth
 	
 	//# may need to be private with getter/setter, other things might change with it
 	public int mineCost = searchDepth-1; //?
@@ -34,8 +46,6 @@ public class Navigator {
 	
 	private MapLocation loc;
 	
-	// int instead of enum for future flexibility
-	// such as combinations of types, e.g. ally and mine
 	private int[][] localMap;
 	
 	
@@ -43,24 +53,65 @@ public class Navigator {
 		this.r = robot;
 		this.localMap = new int[mapSize][mapSize]; // automatically initialized to 0
 	}
+	
+	private Direction[] path;
+	private int pathIdx;
+	private int searchTimer = 0;
+	private MapLocation currentDest;
 
-	public Direction moveTo(MapLocation dest){
+	public Direction moveTo(MapLocation dest) throws GameActionException{
 		
+		if(!dest.equals(currentDest) || (searchTimer %= searchFreq) == 0){
+			updateMap();
+			
+			currentDest = dest;
+			pathIdx = 0;
+			
+			path = search(dest);
+		}
 		
-		return null;
+		searchTimer++;
+		if(!takeStep(path[pathIdx++])){	//## is right? re-search if failed to move?
+			pathIdx--; //# temporary way of handling failed move, improve this
+			return Direction.NONE; // move failed
+		} else if(path[pathIdx] == null) {
+			return Direction.OMNI; // at destination
+		} else {
+			return path[pathIdx];
+		}
+
 	}
 	
+	
+	// Returns success of movement
+	public boolean takeStep(Direction dir) throws GameActionException{
+		if(r.rc.canMove(dir)){
+			MapLocation dest = r.rc.getLocation().add(dir);
+			//#check diffuseMines var first
+			Team mine = r.rc.senseMine(dest);
+			if(mine == Team.NEUTRAL || mine == r.rc.getTeam().opponent()){
+				r.rc.defuseMine(r.rc.getLocation().add(dir));
+			}
+			else{
+				r.rc.move(dir);
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	//## switch to utility version?
 	public int chessDistance(MapLocation loc1, MapLocation loc2){
-		return Math.min(Math.abs(loc1.x - loc2.x), Math.abs(loc1.y - loc2.y));
+		return Math.max(Math.abs(loc1.x - loc2.x), Math.abs(loc1.y - loc2.y));
 	}
 	private int idxDistance(int[] idxs){
-		return Math.min(Math.abs(idxs[0] - offset), Math.abs(idxs[1] - offset));
+		return Math.max(Math.abs(idxs[0] - offset), Math.abs(idxs[1] - offset));
 	}
 	
 	
 	
 	private void updateMap(){
-		loc = r.rc.getLocation(); //## robot property? otherwise nav property? WTF is the bytecode cost anyway?
+		loc = r.rc.getLocation();
 
 		localMap = new int[mapSize][mapSize]; // automatically initialized to 0
 		MapLocation[] nearbyMines = r.rc.senseNonAlliedMineLocations(loc, mapDiagRSquared);
@@ -78,39 +129,76 @@ public class Navigator {
 		return index;
 	}
 	
+	private MapLocation idxToLoc(int[] idx){
+		return new MapLocation(this.loc.x + idx[0] - offset, this.loc.y + idx[1] - offset);
+	}
 	
-	//## test PriorityQueue first?
-	//## return path
 	
 	// Efficiency note: there are priority queue implementations with better asymptotic complexity
 	// (loglog(n) insert/pop); not sure how they compare in this case. There's probably something clever
 	// we can do given that we can bound the number of inserts that will be performed (mapSize^2)
-	private void search(MapLocation goal){
+	private Direction[] search(MapLocation goal){ // returns path
 		
 		boolean[][] visited = new boolean[mapSize][mapSize];
 		
 		LocationQueue frontier = new LocationQueue(mapSize*mapSize);
-		frontier.add(); //## directionTo goal
+		frontier.add(new QueueElement());
 		
-		int[] cur, next;
-		int cost;
+		QueueElement cur;
+		int[] newLoc;
+		int edgeCost, heuristic;
 		while(true){
 			cur = frontier.poll();
-			if(idxDistance(cur) == searchDepth){
-				return 
+			if(idxDistance(cur.idx) == searchDepth || cur.pathLength == maxPathLength){ // is this the best way to limit pathlength?
+				return cur.path;
 			}
 			for(Direction d : Direction.values()){
-				next = {cur[0]+d.dx, cur[1]+d.dx}; //##does this work?
-				if(!visited[next[0]][next[1]]){
-					visited[next[0]][next[1]] = true;
-					cost = curCost + chessDistance(idxToLoc(next), goal) + (localMap[next[0]][next[1]] == 1) ? mineCost : 1; //## change mine detect
-					frontier.add(cost, next);
+				newLoc = new int[]{cur.idx[0]+d.dx, cur.idx[1]+d.dx}; //##does this work?
+				if(!visited[newLoc[0]][newLoc[1]]){
+					heuristic = chessDistance(idxToLoc(newLoc), goal);
+					if(heuristic == 0) {
+						Direction[] goalPath = cur.path;
+						goalPath[cur.pathLength+1] = d;
+						return goalPath;
+					}
+					edgeCost = ((localMap[newLoc[0]][newLoc[1]] == 1) ? mineCost : 1); //@1 Future improvements here
+					frontier.add(new QueueElement(cur, d, edgeCost, heuristic));
+					visited[newLoc[0]][newLoc[1]] = true;
 				}
 			}
 		}
 		
 	}
 	
+	class QueueElement {
+		public int pathLength;
+		public Direction[] path;
+		public int cost;
+		public int priority;
+		public int[] idx;
+
+		// Initial QueueElement
+		QueueElement() {
+			priority = 0;
+			cost = 0;
+			path = new Direction[maxPathLength+1]; // Guaranteed null at end
+			pathLength = 0;
+			idx = new int[] {offset,offset};
+		}
+		
+		QueueElement(QueueElement base, Direction dir, int edgeCost, int heuristic){
+			pathLength = base.pathLength;
+			path = new Direction[maxPathLength];
+			System.arraycopy(base.path, 0, path, 0, pathLength);
+			path[pathLength++] = dir;
+			
+			idx[0] = base.idx[0] + dir.dx;
+			idx[1] = base.idx[1] + dir.dy;
+			cost = base.cost + edgeCost;
+			priority = cost + heuristic;
+		}
+
+	}
 	// ##not locations, rename
 	// Using fixed types for now because generics + arrays is awful
 	class LocationQueue {
@@ -122,14 +210,13 @@ public class Navigator {
 			queue = new QueueElement[maxSize+1];
 		}
 		
-		//## flip inequalities here and poll?
-		public void add(int p, int[] idxs){
-			queue[++end] = new QueueElement(p,idxs);
+		public void add(QueueElement qe){
+			queue[++end] = qe;
 			
 			int curNode = end;
 			int parent;
 			QueueElement tmp;
-			while(curNode > 1 && queue[curNode].priority > queue[parent=curNode/2].priority){
+			while(curNode > 1 && queue[curNode].priority < queue[parent=curNode/2].priority){
 				tmp = queue[curNode];
 				queue[curNode] = queue[parent];
 				queue[parent] = tmp;
@@ -139,8 +226,7 @@ public class Navigator {
 		}
 		
 		// Throws nullPointerException if queue is empty
-		// ## needs to return cost as well
-		public int[] poll(){
+		public QueueElement poll(){
 			QueueElement min = queue[1];
 			queue[1] = queue[end];
 			
@@ -151,9 +237,9 @@ public class Navigator {
 				rp = (r > end) ? 0 : queue[r].priority;
 			int child;
 			QueueElement tmp;
-			while(queue[curNode].priority < lp || queue[curNode].priority < rp){
+			while(queue[curNode].priority > lp || queue[curNode].priority > rp){
 				
-				child = queue[l].priority > queue[r].priority ? l : r;
+				child = queue[l].priority < queue[r].priority ? l : r;
 				
 				tmp = queue[curNode];
 				queue[curNode] = queue[child];
@@ -166,19 +252,9 @@ public class Navigator {
 				rp = (r > end) ? 0 : queue[r].priority;
 			}
 			
-			return min.val;
+			return min;
 		}
 		
 	}
-	
-	class QueueElement {
-		public int priority;
-		public int[] val;
-		QueueElement(int p, int[] idxs){ 
-			priority = p;
-			val = idxs;
-		}
-	}
-	
 	
 }
