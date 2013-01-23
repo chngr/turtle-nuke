169eeb16b3c62e43201023a16e59b2623d29c3ed
paulmusgrave <musgrave.paul1@gmail.com>
2013-01-22 21:13:29 -0500

    added strategy skeleton

---
+++ teams/current/strategies/Strategy.java
@@ -0,0 +1,23 @@
+package current.strategies;
+
+import current.*;
+import battlecode.common.*;
+
+public abstract class Strategy {
+	
+	public HQRobot HQ;
+	
+	public Strategy(HQRobot HQ){
+		this.HQ = HQ;
+	}
+
+	public void evaluatePerformance(){
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	
+}
---
+++ teams/current/strategies/TurtleNuke.java
@@ -0,0 +1,12 @@
+package current.strategies;
+
+import current.*;
+import battlecode.common.*;
+
+public class TurtleNuke extends Strategy {
+
+	public TurtleNuke(HQRobot HQ){
+		super(HQ);
+	}
+	
+}
